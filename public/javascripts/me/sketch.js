a1da98c1588fc9ba07912e2383e585e518052945
wuriyanto-bmd <wuriyanto.wuriyanto.bhinneka.com>
2016-12-28 11:07:11 +0700

    change circle color

--- public/javascripts/me/sketch.js
+++ public/javascripts/me/sketch.js
@@ -6,17 +6,11 @@ var socket;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(0);
-  // Start a socket connection to the server
-  // Some day we would run this server somewhere else
   socket = io.connect('https://dry-badlands-93790.herokuapp.com');
-  // We make a named event called 'mouse' and write an
-  // anonymous callback function
   socket.on('mouse',
-    // When we receive data
     function(data) {
       console.log("Got: " + data.x + " " + data.y);
-      // Draw a blue circle
-      fill('#0080ff');
+      fill(0, 255, 255);
       noStroke();
       ellipse(data.x,data.y, 20, 20);
     }
@@ -24,28 +18,22 @@ function setup() {
 }
 
 function draw() {
-  // Nothing
+
 }
 
 function mouseDragged() {
-  fill('#ff0040');
+  fill(255, 0, 255);
   noStroke();
   ellipse(mouseX,mouseY, 20, 20);
-  // Send the mouse coordinates
   sendMouse(mouseX,mouseY);
 }
 
-// Function for sending to the socket
-function sendMouse(xpos, ypos) {
-  // We are sending!
-  console.log("sendmouse: " + xpos + " " + ypos);
 
-  // Make a little object with  and y
+function sendMouse(xpos, ypos) {
+  console.log("sendmouse: " + xpos + " " + ypos);
   var data = {
     x: xpos,
     y: ypos
   };
-
-  // Send that object to the socket
   socket.emit('mouse',data);
 }
